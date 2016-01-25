b934bcfbba96df7509ba63504b37b4aaae4733fd
xalava <xavier.lavayssiere@gmail.com>
2016-01-25 15:32:49 +0100

    premiersEssais

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,15 @@
 console.log('\'Allo \'Allo!');
+
+d3.csv("data/food.csv", function(data) {
+    console.log(data);
+
+        if (error) {  //If error is not null, something went wrong.
+          console.log(error);  //Log the error.
+        } else {      //If no error, the file loaded correctly. Yay!
+          console.log(data);   //Log the data.
+
+      //Include other code to execute after successful file load here
+
+       }
+
+});
